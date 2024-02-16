56b08813a3c08079118ed64447674d80208a366d
Daniel Evangelista <159594075+Evangelistabv@users.noreply.github.com>
2024-02-16 15:35:55 -0600

    Update settings.py

--- MiTienda/settings.py
+++ MiTienda/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p8+a*jh(#3hw())3u%143vwgy%t38_5frga@m!ec=p@cz1k#gu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
