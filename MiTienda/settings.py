a333d7e748d183b9605145b9f339e4ec217a1243
Daniel Evangelista <159594075+Evangelistabv@users.noreply.github.com>
2024-02-17 17:11:51 -0600

    Update settings.py

--- MiTienda/settings.py
+++ MiTienda/settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 # Obtener las variables de entorno
 SECRET_KEY =  os.environ.get('SECRET_KEY')
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER','danielluffy10@gmail.com')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD','xmquqcjutjrylyog')
-
 from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -154,3 +151,5 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_USE_TLS=True
 EMAIL_port=587
+EMAIL_HOST_USER='danielluffy10@gmail.com'
+EMAIL_HOST_PASSWORD='xmquqcjutjrylyog'
