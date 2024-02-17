0575412c6ce42bd49e8b12762afc2bc42bd5608b
evangelista <danielluffy10@gmail.com>
2024-02-16 20:54:31 -0600

    si

--- MiTienda/settings.py
+++ MiTienda/settings.py
@@ -151,5 +151,3 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_USE_TLS=True
 EMAIL_port=587
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
