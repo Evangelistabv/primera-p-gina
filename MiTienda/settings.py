d0a79c41421c465e8c19249d0e9269c161f590a4
Daniel Evangelista <159594075+Evangelistabv@users.noreply.github.com>
2024-02-16 18:21:16 -0600

    Update settings.py

--- MiTienda/settings.py
+++ MiTienda/settings.py
@@ -136,8 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'MiTiendaApp/static'
+STATIC_URL = 'MiTiendaApp/static/'
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR / 'media'
