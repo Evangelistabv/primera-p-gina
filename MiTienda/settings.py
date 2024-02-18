8ec9d328b57571fea2cafb25011159b44f19cd7c
Daniel Evangelista <159594075+Evangelistabv@users.noreply.github.com>
2024-02-18 15:11:25 -0600

    Update settings.py

--- MiTienda/settings.py
+++ MiTienda/settings.py
@@ -151,5 +151,5 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_USE_TLS=True
 EMAIL_port=587
-EMAIL_HOST_USER='danielluffy10@gmail.com'
-EMAIL_HOST_PASSWORD='xmquqcjutjrylyog'
+EMAIL_HOST_USER=os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD')
