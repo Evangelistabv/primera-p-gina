b7432c77a360ddeeb39875163d5584a015dab986
evangelista <danielluffy10@gmail.com>
2024-02-19 10:55:51 -0600

    sí

--- MiTienda/settings.py
+++ MiTienda/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 # Obtener las variables de entorno
-SECRET_KEY =  os.getenv('SECRET_KEY')
+SECRET_KEY =  os.environ.get('SECRET_KEY')
 from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
