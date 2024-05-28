e44884a12380081e57ea9e61753bda8f6a58f769
Daniel Evangelista <159594075+Evangelistabv@users.noreply.github.com>
2024-05-27 19:49:21 -0600

    Update views.py

--- ContactoApp/views.py
+++ ContactoApp/views.py
@@ -1,26 +1,27 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from ContactoApp.forms import Formulariocontacto
-from django.core.mail import EmailMessage,BadHeaderError,send_mail
-from .forms import Formulariocontacto
-import os
-# Create your views here.
-def contacto(request):
-	formulario=Formulariocontacto()
-	if request.method=='POST':
-		formulario=Formulariocontacto(data=request.POST)
-		if formulario.is_valid():
-			nombre=request.POST.get("nombre")
-			email=request.POST.get("email")
-			contenido=request.POST.get("contenido")
+from django.core.mail import EmailMessage, send_mail
 
-			email=EmailMessage('Mensaje desde App Django',
-				'El usuario {} con direccion {} escribe: \n\n {}'.format(nombre,email,contenido),'',
-				['danielluffy10@gmail.com'])
-			try:
-				send_mail('Mensaje desde App Django',
-				'El usuario {} con direccion {} escribe: \n\n {}'.format(nombre,email,contenido),'',
-				['danielluffy10@gmail.com'])
-			except:
-				error = str(e)
-                		return redirect("/contacto/?error={}".format(error))
-	return render(request,"Contacto/contacto.html",{'miformulario':formulario)
+def contacto(request):
+    formulario = Formulariocontacto()
+    if request.method == 'POST':
+        formulario = Formulariocontacto(data=request.POST)
+        if formulario.is_valid():
+            nombre = request.POST.get("nombre")
+            email = request.POST.get("email")
+            contenido = request.POST.get("contenido")
+
+            email = EmailMessage('Mensaje desde App Django',
+                                 'El usuario {} con dirección {} escribe: \n\n {}'.format(nombre, email, contenido),
+                                 '',
+                                 ['danielluffy10@gmail.com'])
+            try:
+                send_mail('Mensaje desde App Django',
+                          'El usuario {} con direccion {} escribe: \n\n {}'.format(nombre, email, contenido),
+                          '',
+                          ['danielluffy10@gmail.com'])
+            except Exception as e:
+                error = str(e)
+                return redirect("/contacto/?error={}".format(error))
+    return render(request, "Contacto/contacto.html", {'miformulario': formulario})
+
