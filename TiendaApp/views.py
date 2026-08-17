b4a4a84503eda0dc3b07e9bfad96b52eefb61ed4
Evangelistabv <danielluffy10@gmail.com>
2026-08-17 17:23:44 -0600

    Mejora carrito y catalogo de cafeteria

--- TiendaApp/views.py
+++ TiendaApp/views.py
@@ -1,36 +1,52 @@
 from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
 
-from .Carro.carro import Carro
-from .models import Producto
+from TiendaApp.Carro.carro import Carro
+from TiendaApp.models import Producto
 
 
 def tienda(request):
-    productos = Producto.objects.all()
-    return render(request, "Tienda/tienda.html", {"productos": productos})
+    productos = Producto.objects.filter(disponibilidad=True).order_by("id")
+    carro = Carro(request)
+
+    return render(
+        request,
+        "Tienda/tienda.html",
+        {
+            "productos": productos,
+            "carro_items": carro.obtener_items(),
+            "importe_total_carro": carro.total(),
+            "cantidad_total_carro": carro.total_cantidad(),
+        },
+    )
+
+
+def _volver_al_carrito():
+    return redirect(f"{reverse('tienda')}#carrito")
 
 
 def agregar(request, producto_id):
     carro = Carro(request)
-    producto = get_object_or_404(Producto, id=producto_id)
+    producto = get_object_or_404(Producto, id=producto_id, disponibilidad=True)
     carro.agregar(producto=producto)
-    return redirect("tienda")
+    return _volver_al_carrito()
 
 
 def eliminar(request, producto_id):
     carro = Carro(request)
     producto = get_object_or_404(Producto, id=producto_id)
     carro.eliminar(producto=producto)
-    return redirect("tienda")
+    return _volver_al_carrito()
 
 
 def restar(request, producto_id):
     carro = Carro(request)
     producto = get_object_or_404(Producto, id=producto_id)
     carro.restar(producto=producto)
-    return redirect("tienda")
+    return _volver_al_carrito()
 
 
 def limpiar_carro(request):
     carro = Carro(request)
     carro.limpiar_carro()
-    return redirect("tienda")
+    return _volver_al_carrito()
