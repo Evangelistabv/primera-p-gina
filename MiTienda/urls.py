5abe8a7db0eb84ec991a36ea248d296f1ab02ea2
Daniel Evangelista <159594075+Evangelistabv@users.noreply.github.com>
2026-08-17 12:59:49 -0600

    Update urls.py

--- MiTienda/urls.py
+++ MiTienda/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-
+import os
 urlpatterns = [
     path('servicios/',include('ServiciosApp.urls')),
     path('blog/',include('BlogApp.urls')),
