28e25a015ad3b88d1e84466954326ecfa9423945
Daniel Evangelista <159594075+Evangelistabv@users.noreply.github.com>
2026-08-17 13:35:56 -0600

    Update settings.py

--- MiTienda/settings.py
+++ MiTienda/settings.py
@@ -149,7 +149,7 @@ MEDIA_ROOT=BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST='smtp.resend.com'
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
 
